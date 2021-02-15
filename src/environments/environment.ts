c1348ebf823c4a3de60e89bf003ef53d3084304b
Mradul Mishra <mradulmishra@MacBook-Pro.local>
2021-02-16 00:16:58 +0530

    for heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   //apiUrl:"https://exampediaapi-1609247825968.azurewebsites.net"
-  apiUrl:"http://localhost:8080"
+  apiUrl:"https://exampediaapi.herokuapp.com/"
 };
 
 /*
