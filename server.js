52d9dd0c4af3958c6cdcbc6b8bd8cb2f519379c5
BhasmaSur <72976792+BhasmaSur@users.noreply.github.com>
2021-02-16 17:22:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,22 +4,8 @@ const path = require('path');
 //const http = require('http');
 
 const app = express();
-
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/exampedia'));
-
-//app.use(express.static(${__dirname}/exampedia/dist/));
-//app.use(express.static(__dirname + '/dist/exampedia'));
-
-// app.get('/*', (req, res) =>
-//     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
-// );
+app.use(express.static('./dist/ExampediaWeb'));
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/exampedia/'}),
+    res.sendFile('index.html', {root: 'dist/ExampediaWeb/'}),
 );
-
-//app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
-
-//const server = http.createServer(app);
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
