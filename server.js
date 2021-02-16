c5ea444c64b1fe541e81ef711323e0464b45b23d
BhasmaSur <72976792+BhasmaSur@users.noreply.github.com>
2021-02-16 16:48:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const http = require('http');
+//const http = require('http');
 
 const app = express();
 
@@ -14,12 +14,12 @@ app.use(express.static('./dist/exampedia'));
 // app.get('/*', (req, res) =>
 //     res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
 // );
-app.get('*', (req, res) => {
-    res.sendFile(`./exampedia/dist/index.html`); // load the single view file (angular will handle the page changes on the front-end)
-});
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/angular-app-heroku/'}),
+);
 
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+//app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
-const server = http.createServer(app);
+//const server = http.createServer(app);
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
